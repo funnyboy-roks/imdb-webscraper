81346542683301f87c7f416c563ccba538859de3
funnyboy-roks <funnyboyroks@gmail.com>
2021-04-21 10:00:21 -0500

    Add more fields

--- index.js
+++ index.js
@@ -18,8 +18,6 @@ async function getMovieInfo(id) {
 		},
 	});
 
-	fs.writeFileSync(id + '.html', html);
-
 	const dom = new JSDOM(html);
 	const document = dom.window.document;
 	console.log(document.querySelector('.title_wrapper h1').textContent);
@@ -30,13 +28,9 @@ async function getMovieInfo(id) {
 	genres = genres.split(',');
 	const [releaseDateRaw, day, month, year, extraText] = releaseDate.match(/(\d{1,2})([a-z]+)(\d+)(.*)/i);
 
-	let runtimeMins = 0;
-	runtime.match(/\d+[a-z]+/gi).forEach((s) => {
-		if (s.endsWith('h')) {
-			runtimeMins += +s.replace(/h/, '') * 60;
-		} else {
-			runtimeMins += +s.replace(/min/, '');
-		}
+	let stars = [];
+	document.querySelectorAll('div.credit_summary_item:nth-child(4) a').forEach((x) => {
+		x.textContent == 'See full cast & crew' ? '' : stars.push(x.textContent);
 	});
 
 	return {
@@ -45,7 +39,7 @@ async function getMovieInfo(id) {
 		rating: +contentClean('.imdbRating .ratingValue strong span'),
 		contentRating,
 		runtime,
-		runtimeMins,
+		runtimeMins: +((contentClean('div.txt-block:nth-child(20) > time:nth-child(2)') || '0').replace(/min/gi, '')),
 		genres,
 		releaseDate: {
 			rawText: releaseDateRaw,
@@ -55,9 +49,13 @@ async function getMovieInfo(id) {
 			extraText,
 		},
 		image: document.querySelector('.slate_wrapper .poster a img').getAttribute('src'),
+		stars,
+		storyline: content('p:nth-child(1) > span:nth-child(1)').trim(),
+		taglines: content('#titleStoryLine > div:nth-child(8)').textContent.replace('Taglines:', ''),
+		
 	};
 }
 
 // [TestURL:] Singin' in the rain: `tt0045152`
 // [TestURL:] Trial of the Chicago 7: `tt1070874`
-getMovieInfo('tt0045152').then(console.log);
+getMovieInfo('tt1070874').then(console.log);
